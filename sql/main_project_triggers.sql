4ba85616b4e85184bd50a2aed09b4023d3988078
aloisorozco <57361077+aloisorozco@users.noreply.github.com>
2023-08-05 08:31:22 -0400

    added trigger for teacher infected
    there is still the email part to do for this trigger will add soon

--- sql/main_project_triggers.sql
+++ sql/main_project_triggers.sql
@@ -1,1 +1,20 @@
-
+-- Infection Trigger
+CREATE TRIGGER TeacherInfectedTrigger
+  AFTER INSERT ON Infections
+  FOR EACH ROW
+  BEGIN
+    IF NEW.personID IN (
+        SELECT personID
+          FROM EmployeeRegistrations AS er
+          JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+          WHERE et.employeeType = 'Teacher'
+      )
+      THEN  
+        INSERT INTO Emails -- TODO;
+        DELETE FROM Assignments AS a
+          WHERE a.teacherID = NEW.personID
+          AND dueDate <= DATE_ADD(CURRENT_DATE(), INTERVAL +14 DAY)
+          AND dueDate >= CURRENT_DATE();
+    END IF;
+  END;
+    
