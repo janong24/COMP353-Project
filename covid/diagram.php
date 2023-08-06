b4e1ca3b6e93f9451160b57bfabdec59d170407e
Jan Ong <janong24@gmail.com>
2023-08-05 22:37:48 -0400

    Embedded E/R diagram

---
+++ covid/diagram.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles.css">
+  <script src="https://kit.fontawesome.com/6ebd7b3ed7.js" crossorigin="anonymous"></script>
+  <title>E/R Diagram</title>
+</head>
+<body>
+  <?php include_once ('../navbar.php'); ?>
+  <div class="absCenter">
+    <h1>E/R Diagram</h1>
+    <p>Zoom or drag to explore the diagram.</p>
+    <iframe width="800" height="480" src='https://dbdiagram.io/embed/64b7797d02bd1c4a5e50ee76'%3E </iframe>
+  </div>
+</body>
+</html>
