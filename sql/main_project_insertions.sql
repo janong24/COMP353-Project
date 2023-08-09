83a4b27bac254f5b9d1fc540e0ee0f0c305ee5d6
junkokie <76203567+junkokie@users.noreply.github.com>
2023-08-09 02:09:15 -0400

    Add files via upload
    added insertions according to queries 8-11, 13-17.

--- sql/main_project_insertions.sql
+++ sql/main_project_insertions.sql
@@ -1,4 +1,3 @@
-
 -- Populating Tables
 
 
@@ -39,57 +38,83 @@ INSERT INTO Persons VALUES(25, 'Mike', 'Teller', 2018-10-23, 'TELL10102387', 202
 INSERT INTO Persons VALUES(26, 'Ben', 'Dover', 2010-10-23, 'DOVER10102387', 2026-10-23, '514-121-1212', '6833 boul. Picodon', 'Montreal', 'QC', 'H1X8Q3', 'Canada', 'bendover@gmail.com');
 INSERT INTO Persons VALUES(27, 'Mike', 'Hauck', 2010-10-23, 'HAUCK10102387', 2026-10-23, '514-121-1212', '6833 boul. Picodon', 'Montreal', 'QC', 'H1X8Q3', 'Canada', 'mikehauck@gmail.com');
 
+-- for q9
+INSERT INTO Persons VALUES(28, 'Jim', 'Biden', DATE('2017-09-06'), 'TEST', DATE('2028-09-06'), '514-512-5151', '72 Test Street', 'Belleville', 'ON', 'K0B1D4', 'Germany', 'test@gmail.com');
+INSERT INTO Persons VALUES(29, 'Joe', 'Biden', DATE('2017-09-06'), 'TEST', DATE('2028-09-06'), '514-512-5151', '72 Test Street', 'Belleville', 'ON', 'K0B1D4', 'Germany', 'test@gmail.com');
+INSERT INTO Persons VALUES(30, 'Jack', 'Biden', DATE('2017-09-06'), 'TEST', DATE('2028-09-06'), '514-512-5151', '72 Test Street', 'Belleville', 'ON', 'K0B1D4', 'Germany', 'test@gmail.com');
+INSERT INTO Persons VALUES(31, 'Joseph', 'Biden', DATE('2017-09-06'), 'TEST', DATE('2028-09-06'), '514-512-5151', '72 Test Street', 'Belleville', 'ON', 'K0B1D4', 'Germany', 'test@gmail.com');
+
+-- q16 two more Ministers of Education
+INSERT INTO Persons VALUES(32, 'Liana', 'Bartlett', '1970-12-15', 'LIAB28115564', '2028-11-11', '514-859-8596', '773 Richmond Rd', 'Richmond', 'BC', 'V5C1J7', 'Canada', 'lianabartlett@gmail.com');
+INSERT INTO Persons VALUES(33, 'Keith', 'Bryan', '1981-11-05', 'KEBR81115514', '2028-05-05', '514-441-2777', '91 Henry Blvd', 'Richmond', 'BC', 'V5K0J1', 'Canada', 'keithbryan@gmail.com');
+
 -- End of Persons--
 
 -- Infections(InfectionID, DateOfInfection, TypeID [(1, COVID-19), (2, SARS-CoV-2 Variant), (3, Other)], PersonID)--
-INSERT INTO Infections VALUES(1, 2023-05-06, 1, 1);
-INSERT INTO Infections VALUES(2, 2023-05-06, 1, 3);
-INSERT INTO Infections VALUES(3, 2023-05-07, 2, 5);
-INSERT INTO Infections VALUES(4, 2023-05-08, 2, 7);
-INSERT INTO Infections VALUES(5, 2023-05-08, 3, 9);
-INSERT INTO Infections VALUES(6, 2023-05-08, 1, 11);
-INSERT INTO Infections VALUES(7, 2023-05-08, 2, 13);
-INSERT INTO Infections VALUES(8, 2023-05-08, 2, 15);
-INSERT INTO Infections VALUES(9, 2023-05-09, 1, 17);
-INSERT INTO Infections VALUES(10, 2023-05-09, 1, 19);
+INSERT INTO Infections VALUES(1, DATE('2023-05-06'), 1, 1);
+INSERT INTO Infections VALUES(2, DATE('2023-05-06'), 1, 3);
+INSERT INTO Infections VALUES(3, DATE('2023-05-07'), 2, 5);
+INSERT INTO Infections VALUES(4, DATE('2023-05-08'), 2, 7);
+INSERT INTO Infections VALUES(5, DATE('2023-05-08'), 3, 9);
+INSERT INTO Infections VALUES(6, DATE('2023-05-08'), 1, 11);
+INSERT INTO Infections VALUES(7, DATE('2023-05-08'), 2, 13);
+INSERT INTO Infections VALUES(8, DATE('2023-05-08'), 2, 15);
+INSERT INTO Infections VALUES(9, DATE('2023-05-09'), 1, 17);
+INSERT INTO Infections VALUES(10, DATE('2023-05-09'), 1, 19);
 -- test--
-INSERT INTO Infections VALUES(11, 2023-06-09, 1, 14);
-INSERT INTO Infections VALUES(12, 2023-07-19, 1, 23);
-INSERT INTO Infections VALUES(13, 2023-07-20, 2, 23);
-INSERT INTO Infections VALUES(14, 2023-07-19, 1, 24);
-INSERT INTO Infections VALUES(15, 2023-07-20, 2, 24);
+INSERT INTO Infections VALUES(11, DATE('2023-06-09'), 1, 14);
+INSERT INTO Infections VALUES(12, DATE('2023-07-19'), 1, 23);
+INSERT INTO Infections VALUES(13, DATE('2023-07-20'), 2, 23);
+INSERT INTO Infections VALUES(14, DATE('2023-07-19'), 1, 24);
+INSERT INTO Infections VALUES(15, DATE('2023-07-20'), 2, 24);
 --
-INSERT INTO Infections VALUES(16, 2023-07-25, 1, 14);
-INSERT INTO Infections VALUES(17, 2023-07-29, 1, 20);
-INSERT INTO Infections VALUES(18, 2023-08-02, 1, 21);
-INSERT INTO Infections VALUES(19, 2023-08-09, 1, 17);
-INSERT INTO Infections VALUES(20, 2023-08-10, 1, 2);
+INSERT INTO Infections VALUES(16, DATE('2023-07-25'), 1, 14);
+INSERT INTO Infections VALUES(17, DATE('2023-07-29'), 1, 20);
+INSERT INTO Infections VALUES(18, DATE('2023-08-02'), 1, 21);
+INSERT INTO Infections VALUES(19, DATE('2023-08-09'), 1, 17);
+INSERT INTO Infections VALUES(20, DATE('2023-08-10'), 1, 2);
+
+-- for q17, employee got infected >= 3 times
+INSERT INTO Infections VALUES(21, DATE('2021-10-02'), 1, 1);
+INSERT INTO Infections VALUES(22, DATE('2021-11-09'), 1, 1);
+INSERT INTO Infections VALUES(23, DATE('2021-12-10'), 1, 1);
+INSERT INTO Infections VALUES(24, DATE('2021-07-11'), 1, 3);
+INSERT INTO Infections VALUES(25, DATE('2021-09-20'), 1, 3);
+INSERT INTO Infections VALUES(26, DATE('2021-07-11'), 1, 5);
+INSERT INTO Infections VALUES(27, DATE('2021-09-20'), 1, 5);
+INSERT INTO Infections VALUES(28, DATE('2021-07-11'), 1, 7);
+INSERT INTO Infections VALUES(29, DATE('2021-09-20'), 1, 7);
+INSERT INTO Infections VALUES(30, DATE('2021-07-11'), 1, 10);
+INSERT INTO Infections VALUES(31, DATE('2022-06-20'), 1, 10);
+INSERT INTO Infections VALUES(32, DATE('2022-07-21'), 1, 10);
 
 -- Vaccinations(VaccinationID, VaccinationDate, DoseNumber, VaccineID [(1, Pfizer), (2, Moderna), (3, AztraZeneca), (4, Johnson & Johnson)], PersonID)--
-INSERT INTO Vaccinations VALUES(1, 2019-04-04, 1, 1, 2);
-INSERT INTO Vaccinations VALUES(2, 2019-04-04, 1, 1, 4);
-INSERT INTO Vaccinations VALUES(3, 2019-04-05, 1, 2, 6);
-INSERT INTO Vaccinations VALUES(4, 2019-04-05, 1, 3, 8);
-INSERT INTO Vaccinations VALUES(5, 2019-04-06, 1, 4, 10);
-INSERT INTO Vaccinations VALUES(6, 2019-06-04, 2, 1, 2);
-INSERT INTO Vaccinations VALUES(7, 2019-06-04, 2, 1, 4);
-INSERT INTO Vaccinations VALUES(8, 2019-04-05, 1, 4, 12);
-INSERT INTO Vaccinations VALUES(9, 2019-04-05, 1, 3, 16);
-INSERT INTO Vaccinations VALUES(10, 2019-04-06, 2, 4, 12);
-
---inserting more data for testing on schedules
-INSERT INTO Vaccinations VALUES(11, 2023-04-06, 1, 4, 5);
-INSERT INTO Vaccinations VALUES(12, 2023-05-06, 1, 2, 7);
-INSERT INTO Vaccinations VALUES(13, 2023-06-06, 2, 4, 5);
-INSERT INTO Vaccinations VALUES(14, 2023-07-02, 1, 2, 25);
-INSERT INTO Vaccinations VALUES(15, 2023-07-03, 1, 2, 26);
-INSERT INTO Vaccinations VALUES(16, 2023-07-03, 2, 4, 10);
-
+INSERT INTO Vaccinations VALUES(1, DATE('2019-04-04'), 1, 1, 2);
+INSERT INTO Vaccinations VALUES(2, DATE('2019-04-04'), 1, 1, 4);
+INSERT INTO Vaccinations VALUES(3, DATE('2019-04-05'), 1, 2, 6);
+INSERT INTO Vaccinations VALUES(4, DATE('2019-04-05'), 1, 3, 8);
+INSERT INTO Vaccinations VALUES(5, DATE('2019-04-06'), 1, 4, 10);
+INSERT INTO Vaccinations VALUES(6, DATE('2019-06-04'), 2, 1, 2);
+INSERT INTO Vaccinations VALUES(7, DATE('2019-06-04'), 2, 1, 4);
+INSERT INTO Vaccinations VALUES(8, DATE('2019-04-05'), 1, 4, 12);
+INSERT INTO Vaccinations VALUES(9, DATE('2019-04-05'), 1, 3, 16);
+INSERT INTO Vaccinations VALUES(10, DATE('2019-04-06'), 2, 4, 12);
+INSERT INTO Vaccinations VALUES(11, DATE('2023-04-06'), 1, 4, 5);
+INSERT INTO Vaccinations VALUES(12, DATE('2023-05-06'), 1, 2, 7);
+INSERT INTO Vaccinations VALUES(13, DATE('2023-06-06'), 2, 4, 5);
+INSERT INTO Vaccinations VALUES(14, DATE('2023-07-02'), 1, 2, 25);
+INSERT INTO Vaccinations VALUES(15, DATE('2023-07-03'), 1, 2, 26);
+INSERT INTO Vaccinations VALUES(16, DATE('2023-07-03'), 2, 4, 10);
+INSERT INTO Vaccinations VALUES(17, DATE('2023-07-03'), 2, 2, 6);
+INSERT INTO Vaccinations VALUES(18, DATE('2023-07-04'), 1, 1, 27);
+INSERT INTO Vaccinations VALUES(19, DATE('2023-07-04'), 1, 1, 11);
 
 -- Ministries(MinistryID, Name, Address, City, Province, PostalCode, PhoneNumber, WebAddress, Capacity, MinisterOfEducationID)--
 INSERT INTO Ministries VALUES(1, 'Laval Ministry of Education', '1201 ch. du Omelette', 'Laval', 'QC', 'H1P7U9', '514-999-8855', 'https://laval.education.qc.ca', 15000, 21);
 INSERT INTO Ministries VALUES(2, 'Montreal Ministry of Education', '4812 rue Fromage', 'Montreal', 'QC', 'H3X7H1', '514-111-2244', 'https://montreal.education.qc.ca', 40000, 22);
 INSERT INTO Ministries VALUES(3, 'West Island Ministry of Education', '157 boul. Camembert', 'Pierrefonds', 'QC', 'H7Q1E4', '514-149-4317', 'https://westisland.education.qc.ca', 25000, 23);
+INSERT INTO Ministries VALUES(4, 'Brossard Ministry of Education', '1879 Bd Taschereau', 'Brossard', 'QC', 'J4X1A0', '514-599-4667', 'https://brossard.education.qc.ca', 28000, 32);
+INSERT INTO Ministries VALUES(5, 'La Prairie Ministry of Education', '15 rue Saint-Georges', 'La Prairie', 'QC', 'J5R0B1', '514-359-4334', 'https://laprairie.education.qc.ca', 5000, 33);
 
 -- Facilities(FacilityID, Name, Address, City, Province, PostalCode, PhoneNumber, WebAddress, Capacity, MinistryID, FacilityTypeID)--
 INSERT INTO Facilities VALUES(1, 'Rosemont Elementary School', '123 Av. Rosemont', 'Montreal', 'QC', 'H7H8K0', '514-134-4513', 'https://ecolerosemont.edu', 2500, 2, 3);
@@ -103,42 +128,51 @@ INSERT INTO Facilities VALUES(8, 'West Island English School Board ', '500 Swiss
 -- test--
 INSERT INTO Facilities VALUES(9, 'Mog English School Board ', '420 Rue Trichard', 'Pierrefonds', 'QC', 'HB01M3', '514-357-9455', 'https://montreal.education.qc.ca/board', 100, 3, 1);
 INSERT INTO Facilities VALUES(10, 'New England English School Board ', '69 Rue Mane', 'Pierrefonds', 'QC', 'HB01M3', '514-347-9455', 'https://montreal.education.qc.ca/board', 100, 3, 1);
+INSERT INTO Facilities VALUES(11, 'Laval Junior English School Board ', '70 Rue Vallier', 'Laval', 'QC', 'H2S2M2', '514-087-9441', 'https://lavaljunior.education.qc.ca/board', 200, 1, 2);
+INSERT INTO Facilities VALUES(12, 'Brossard English School Board ', '80 Rue Marie', 'Brossard', 'QC', 'J4R1H3', '514-117-9625', 'https://brossard.education.qc.ca/board', 150, 4, 1);
 
 -- StudentRegistrations(PersonID, FacilityID, StartDate, EndDate, SchoolTypeID, SchoolLevel)--
-INSERT INTO StudentRegistrations VALUES(11, 5, '2023-09-01', NULL, 2, 6);
-INSERT INTO StudentRegistrations VALUES(12, 4, '2020-09-01', NULL, 2, 5);
-INSERT INTO StudentRegistrations VALUES(13, 3, '2022-09-01', NULL, 1, 4);
-INSERT INTO StudentRegistrations VALUES(14, 1, '2020-09-01', NULL, 1, 3);
-INSERT INTO StudentRegistrations VALUES(15, 2, '2019-09-01', NULL, 1, 2);
-INSERT INTO StudentRegistrations VALUES(16, 5, '2023-09-01', NULL, 2, 6);
-INSERT INTO StudentRegistrations VALUES(17, 4, '2020-09-01', NULL, 2, 5);
-INSERT INTO StudentRegistrations VALUES(18, 3, '2022-09-01', NULL, 1, 4);
-INSERT INTO StudentRegistrations VALUES(19, 1, '2019-09-01', NULL, 1, 3);
-INSERT INTO StudentRegistrations VALUES(20, 2, '2020-09-01', NULL, 1, 2);
+INSERT INTO StudentRegistrations VALUES(11, 5, DATE('2023-09-01'), NULL, 2, 6);
+INSERT INTO StudentRegistrations VALUES(12, 4, DATE('2020-09-01'), NULL, 2, 5);
+INSERT INTO StudentRegistrations VALUES(13, 3, DATE('2022-09-01'), NULL, 1, 4);
+INSERT INTO StudentRegistrations VALUES(14, 1, DATE('2020-09-01'), NULL, 1, 3);
+INSERT INTO StudentRegistrations VALUES(15, 2, DATE('2019-09-01'), NULL, 1, 2);
+INSERT INTO StudentRegistrations VALUES(16, 5, DATE('2023-09-01'), NULL, 2, 6);
+INSERT INTO StudentRegistrations VALUES(17, 4, DATE('2020-09-01'), NULL, 2, 5);
+INSERT INTO StudentRegistrations VALUES(18, 3, DATE('2022-09-01'), NULL, 1, 4);
+INSERT INTO StudentRegistrations VALUES(19, 1, DATE('2019-09-01'), NULL, 1, 3);
+INSERT INTO StudentRegistrations VALUES(20, 2, DATE('2020-09-01'), NULL, 1, 2);
 
 -- test--
-INSERT INTO StudentRegistrations VALUES(23, 6, 2020-09-01, NULL,1, 3);
-INSERT INTO StudentRegistrations VALUES(24, 6, 2020-09-01, NULL,2, 3);
+INSERT INTO StudentRegistrations VALUES(23, 6, DATE('2020-09-01'), NULL, 1, 3);
+INSERT INTO StudentRegistrations VALUES(24, 6, DATE('2020-09-01'), NULL, 2, 3);
 
 -- EmployeeRegistrations(PersonID, FacilityID, StartDate, EndDate, EmployeeTypeID, SpecializationID)--
-INSERT INTO EmployeeRegistrations VALUES(1, 8, '2015-06-01', '2019-12-30', 1, 1);
-INSERT INTO EmployeeRegistrations VALUES(1, 6, '2020-08-01', NULL, 2, NULL);
-INSERT INTO EmployeeRegistrations VALUES(2, 7, '2020-11-25', NULL, 1, 2);
-INSERT INTO EmployeeRegistrations VALUES(3, 7, '2016-07-15', '2021-06-25', 2, NULL);
-INSERT INTO EmployeeRegistrations VALUES(3, 8, '2021-12-28', NULL, 3, NULL);
-INSERT INTO EmployeeRegistrations VALUES(4, 6, '2022-02-17', NULL, 1, 3);
-INSERT INTO EmployeeRegistrations VALUES(5, 4, '2012-07-15', '2015-07-16', 3, NULL);
-INSERT INTO EmployeeRegistrations VALUES(5, 3, '2015-09-01', '2022-01-03', 1, 2);
-INSERT INTO EmployeeRegistrations VALUES(5, 1, '2022-04-20', NULL, 1, 1);
-INSERT INTO EmployeeRegistrations VALUES(6, 2, '2022-07-27', NULL, 3, NULL);
-INSERT INTO EmployeeRegistrations VALUES(7, 1, '2019-06-15', '2022-09-01', 1, 1);
-INSERT INTO EmployeeRegistrations VALUES(7, 3, '2022-11-05', NULL, 2, NULL);
-INSERT INTO EmployeeRegistrations VALUES(8, 4, '2023-07-31', NULL, 1, 3);
-INSERT INTO EmployeeRegistrations VALUES(9, 2, '2020-06-30', '2022-07-30', 2, NULL);
-INSERT INTO EmployeeRegistrations VALUES(9, 5, '2022-09-18', NULL, 1, 4);
-INSERT INTO EmployeeRegistrations VALUES(10, 7, '2022-11-30', NULL, 1, 5);
-INSERT INTO EmployeeRegistrations VALUES(26, 9, '2023-01-01', NULL, 5, NULL);
-INSERT INTO EmployeeRegistrations VALUES(25, 10, '2023-01-01', NULL, 5, NULL);
+INSERT INTO EmployeeRegistrations VALUES(1, 8, DATE('2015-06-01'), DATE('2019-12-30'), 1, 1);
+INSERT INTO EmployeeRegistrations VALUES(1, 6, DATE('2020-08-01'), NULL, 2, NULL);
+INSERT INTO EmployeeRegistrations VALUES(2, 7, DATE('2020-11-25'), NULL, 1, 2);
+INSERT INTO EmployeeRegistrations VALUES(3, 7, DATE('2016-07-15'), DATE('2021-06-25'), 2, NULL);
+INSERT INTO EmployeeRegistrations VALUES(3, 8, DATE('2021-12-28'), NULL, 3, NULL);
+INSERT INTO EmployeeRegistrations VALUES(4, 6, DATE('2022-02-17'), NULL, 1, 3);
+INSERT INTO EmployeeRegistrations VALUES(5, 4, DATE('2012-07-15'), DATE('2015-07-16'), 3, NULL);
+INSERT INTO EmployeeRegistrations VALUES(5, 3, DATE('2015-09-01'), DATE('2022-01-03'), 1, 2);
+INSERT INTO EmployeeRegistrations VALUES(5, 1, DATE('2022-04-20'), NULL, 1, 1);
+INSERT INTO EmployeeRegistrations VALUES(6, 2, DATE('2022-07-27'), NULL, 3, NULL);
+INSERT INTO EmployeeRegistrations VALUES(7, 1, DATE('2019-06-15'), DATE('2022-09-01'), 1, 1);
+INSERT INTO EmployeeRegistrations VALUES(7, 3, DATE('2022-11-05'), NULL, 2, NULL);
+INSERT INTO EmployeeRegistrations VALUES(8, 1, DATE('2023-07-31'), NULL, 1, 3);
+INSERT INTO EmployeeRegistrations VALUES(9, 2, DATE('2020-06-30'), DATE('2022-07-30'), 2, NULL);
+INSERT INTO EmployeeRegistrations VALUES(9, 5, DATE('2022-09-18'), NULL, 1, 4);
+INSERT INTO EmployeeRegistrations VALUES(10, 7, DATE('2022-11-30'), NULL, 1, 5);
+INSERT INTO EmployeeRegistrations VALUES(26, 9, DATE('2023-01-01'), NULL, 5, NULL);
+INSERT INTO EmployeeRegistrations VALUES(25, 10, DATE('2023-01-01'), NULL, 5, NULL);
+INSERT INTO EmployeeRegistrations VALUES(27, 1, DATE('2023-01-01'), NULL, 3, NULL);
+-- for q9
+INSERT INTO EmployeeRegistrations VALUES(28, 1, DATE('2023-01-01'), NULL, 5, NULL);
+INSERT INTO EmployeeRegistrations VALUES(29, 1, DATE('2023-01-01'), NULL, 5, NULL);
+INSERT INTO EmployeeRegistrations VALUES(30, 1, DATE('2023-01-01'), NULL, 5, NULL);
+INSERT INTO EmployeeRegistrations VALUES(12, 3, DATE('2023-01-01'), NULL, 1, 5);
+INSERT INTO EmployeeRegistrations VALUES(16, 7, DATE('2023-01-01'), NULL, 3, NULL);
 
 -- Schedules(scheduleID, personID, facilityID, startTime, endTime)
 INSERT INTO Schedules VALUES(1, 25, 10, '2012-08-13 11:39:51', '2012-08-13 17:39:51');
@@ -148,7 +182,7 @@ INSERT INTO Schedules VALUES(4, 5, 1, '2023-08-03 11:39:51', '2023-08-03 17:39:5
 INSERT INTO Schedules VALUES(5, 7, 1, '2023-08-03 11:39:51', '2023-08-03 17:39:51');
 INSERT INTO Schedules VALUES(6, 7, 1, '2023-08-04 11:39:51', '2023-08-04 17:39:51');
 
--- Inserting more data into the Schedules table
+-- Inserting more data 
 INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (7, 25, 10, '2023-08-04 09:00:00', '2023-08-04 17:00:00');
 INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (8, 5, 1, '2023-08-04 09:30:00', '2023-08-04 16:30:00');
 INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (9, 25, 10, '2023-08-05 10:00:00', '2023-08-05 18:00:00');
@@ -167,8 +201,22 @@ INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VAL
 INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (22, 10, 7, '2023-08-26 10:00:00', '2023-08-20 18:00:00');
 INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (23, 10, 7, '2023-08-28 09:00:00', '2023-08-20 21:00:00');
 INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (24, 26, 9, '2023-08-29 11:00:00', '2023-08-20 17:00:00');
+-- testing invaccinated employee
+INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (25, 9, 5, '2023-08-29 11:00:00', '2023-08-29 17:00:00');
+-- testing infected employee
+INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (26, 2, 7, '2023-08-11 11:00:00', '2023-08-11 17:00:00');
+
+-- for q13 q14
+INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (27, 8, 1, '2023-08-06 09:00:00', '2023-08-06 17:00:00');
+INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (28, 27, 1, '2023-08-08 10:00:00', '2023-08-08 18:00:00');
+
+-- for q17
+INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (29, 1, 6, '2023-08-08 07:00:00', '2023-08-08 20:00:00');
+INSERT INTO Schedules (scheduleID, personID, facilityID, startTime, endTime) VALUES (30, 3, 8, '2022-08-08 07:00:00', '2022-08-08 17:00:00');
+
 
 
 -- EmailContent(emailContentID, emailSubject, emailBody, emailDate)
+
 
 -- EmailLogs(emailContentID, facilityID, emailReceiverID)
