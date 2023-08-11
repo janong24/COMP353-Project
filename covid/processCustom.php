ffa6ba83a083ec8ccc5ddef99f6a4940e16e9e48
Jan Ong <janong24@gmail.com>
2023-08-10 21:32:44 -0400

    Refactored custom.php

---
+++ covid/processCustom.php
@@ -0,0 +1,309 @@
+<?php require_once '../database.php';
+require_once '../functions.php';
+
+if(isset($_POST["query"]))  {
+  $query = $_POST["query"];
+} else {
+  $query = "";
+}
+
+if(isset($_POST["facility"]))  {
+  $facility = $_POST["facility"];
+} else {
+  $facility = "";
+}
+
+if(isset($_POST["employee"]))  {
+  $employee = $_POST["employee"];
+} else {
+  $employee = "";
+}
+
+$facilities = (getFacilities());
+$employees = (getEmployees());
+
+//variable initialization
+$specificPeriodStartTime = DATE('2012-01-01');
+$specificPeriodEndTime = DATE('2022-01-01');
+
+// SQL queries
+switch ($query) {
+  case "invalid":
+    echo '<script>alert("Invalid choice.")</script>';
+    $query = "";
+    break;
+  case "q8":
+    $query = "
+    SELECT f.name, f.address, f.city, f.province, f.postalCode, f.phoneNumber, f.webAddress, ft.typeName, r.totalEmployees, r2.principalFirstName, r2.principalLastName
+    FROM Facilities AS f
+    JOIN FacilityTypes AS ft ON f.facilityTypeID = ft.typeID
+    JOIN (
+      SELECT er.facilityID, COUNT(er.personID) AS 'totalEmployees'
+        FROM EmployeeRegistrations AS er
+        JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+        GROUP BY er.facilityID
+    ) r ON f.facilityID = r.FacilityID
+    LEFT JOIN (
+      SELECT er.facilityID, et.employeeType, p.firstName AS 'principalFirstName', p.lastName AS 'principalLastName'
+        FROM EmployeeRegistrations AS er
+        JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+        JOIN Persons AS p ON er.personID = p.personID
+        WHERE employeeType = 'Principal' OR employeeType = 'President'
+    ) r2 ON f.facilityID = r2.facilityID
+    ORDER BY f.province ASC, f.city ASC, ft.typeName ASC, r.totalEmployees ASC;
+    ";
+    break;
+  case "q9":
+    $query = "
+    SELECT p.firstName, p.lastName, er.startDate, p.dateOfBirth, p.medicareNumber, p.telephoneNumber, p.address, p.city, p.province, p.postalCode, p.citizenship, p.email
+    FROM Facilities AS f
+    JOIN EmployeeRegistrations AS er ON f.facilityID = er.facilityID
+    JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+    JOIN Persons AS p ON er.personID = p.personID
+    WHERE f.facilityID = " . $facility . "
+    ORDER BY et.employeeType ASC, p.firstName ASC, p.lastName ASC;
+    ";
+    break;
+  case "q10":
+    $query = "
+    SELECT f.name, DATE(s.startTime) AS 'dayOfTheYear', s.startTime, s.endTime
+    FROM Schedules AS s
+    JOIN Facilities AS f ON s.facilityID = f.facilityID
+    WHERE DATE(s.startTime) >= " . $specificPeriodStartTime . "
+    AND DATE(s.endTime) <= " . $specificPeriodEndTime . "
+    AND s.personID = " . $employee . "
+    ORDER BY f.name, DATE(s.startTime), s.startTime;
+    ";
+    break;
+  case "q11":
+    $query = "
+    SELECT p.firstName, p.lastName, i.dateOfInfection, f.name
+    FROM Persons AS p
+    JOIN EmployeeRegistrations AS er ON p.personID = er.personID
+    JOIN Facilities AS f ON er.facilityID = f.facilityID
+    JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+    JOIN Infections AS i ON p.personID = i.personID
+    JOIN TypeOfInfections AS ti on i.TypeID = ti.TypeID
+    WHERE et.employeeType = 'Teacher'
+        AND ti.TypeName = 'COVID-19'
+        AND i.dateOfInfection BETWEEN DATE_SUB(CURDATE(), INTERVAL 2 WEEK) AND CURDATE()
+    ORDER BY f.name, p.firstName;
+    ";
+    break;
+  case "q12":
+    $query = "
+    SELECT *
+    FROM EmailContent AS ec
+    JOIN EmailLogs AS el ON ec.emailContentID = el.emailContentID
+    WHERE el.facilityID = " . $facility . "
+    ORDER BY ec.emailDate;
+    ";
+    break;
+  case "q13":
+    $query = "
+    SELECT s.facilityID, p.firstName, p.lastName,
+    CASE 
+      WHEN ft.SubTypeName IN ('Primary School', 'Middle School') THEN 'elementary'
+      ELSE 'secondary'
+    END AS 'role'
+    FROM Schedules AS s
+    JOIN Persons AS p ON s.personID = p.personID
+    JOIN Facilities AS f ON s.facilityID = f.facilityID
+    JOIN FacilityTypes AS ft ON f.facilityTypeID = ft.typeID
+    JOIN EmployeeRegistrations AS er ON p.personID = er.personID
+    JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+    WHERE et.employeeType = 'Teacher'
+    AND DATE(s.startTime) BETWEEN DATE_ADD(CURRENT_DATE(), INTERVAL -14 DAY) AND CURRENT_DATE()
+    AND s.facilityID = specificFacilityID " . $facility . "
+    GROUP BY p.personID
+    ORDER BY 
+      CASE 
+        WHEN ft.SubTypeName IN ('Primary School', 'Middle School') THEN 1
+        ELSE 2
+      END, p.firstName;
+    ";
+    break;
+  case "q14":
+    $query = "
+    SELECT s.facilityID, p.personID, SUM(TIMESTAMPDIFF(HOUR, s.startTime, s.endTime)) AS 'totalHours', s.startTime, s.endTime
+    FROM Schedules AS s
+    JOIN Persons AS p ON s.personID = p.personID
+    JOIN EmployeeRegistrations AS er ON p.personID = er.personID
+    JOIN EmployeeType AS et ON er.employeeTypeID = et.employeeTypeID
+    WHERE et.employeeType = 'Teacher'
+    AND DATE(s.startTime) >= " . $specificPeriodStartTime . "
+    AND DATE(s.endTime) <= " . $specificPeriodEndTime . "
+    AND s.facilityID = " . $facility . "
+    GROUP BY p.personID, p.firstName, p.lastName
+    ORDER BY p.firstName, p.lastName;
+    ";
+    break;
+  case "q15":
+    $query = "
+    SELECT f.Province, f.Name AS SchoolName, f.Capacity, IFNULL(ti.TeacherCount, 0) AS TeachersInfectedCount, IFNULL(si.StudentCount, 0) AS StudentsInfectedCount
+    FROM Facilities f
+    LEFT JOIN (
+      SELECT 
+          f2.FacilityID, 
+          COUNT(DISTINCT e.PersonID) AS TeacherCount
+      FROM Facilities f2
+      JOIN EmployeeRegistrations e ON f2.FacilityID = e.FacilityID
+      JOIN Infections i ON e.PersonID = i.PersonID
+      WHERE f2.FacilityTypeID = (SELECT TypeID FROM FacilityTypes WHERE SubTypeName = 'High School')
+        AND i.DateOfInfection >= CURDATE() - INTERVAL 2 WEEK
+        AND i.TypeID = (SELECT TypeID FROM TypeOfInfections WHERE TypeName = 'COVID-19')
+        AND e.EmployeeTypeID = (SELECT EmployeeTypeID FROM EmployeeType WHERE EmployeeType = 'Teacher')
+      GROUP BY f2.FacilityID
+    ) AS ti ON f.FacilityID = ti.FacilityID
+    LEFT JOIN (
+      SELECT 
+          f3.FacilityID, 
+          COUNT(DISTINCT s.PersonID) AS StudentCount
+      FROM Facilities f3
+      JOIN StudentRegistrations s ON f3.FacilityID = s.FacilityID
+      JOIN Infections i ON s.PersonID = i.PersonID
+      WHERE f3.FacilityTypeID = (SELECT TypeID FROM FacilityTypes WHERE SubTypeName = 'High School')
+        AND i.DateOfInfection >= CURDATE() - INTERVAL 2 WEEK
+        AND i.TypeID = (SELECT TypeID FROM TypeOfInfections WHERE TypeName = 'COVID-19')
+        AND s.SchoolTypeID = (SELECT SchoolTypeID FROM SchoolType WHERE SchoolType = 'High School')
+      GROUP BY f3.FacilityID
+    ) AS si ON f.FacilityID = si.FacilityID
+    WHERE f.FacilityTypeID = (SELECT TypeID FROM FacilityTypes WHERE SubTypeName = 'High School')
+    ORDER BY f.Province ASC, IFNULL(ti.TeacherCount, 0) ASC;
+    ";
+    break;
+  case "q16":
+    $query = "
+    SELECT p.FirstName, p.LastName, p.City, IFNULL(r.numberOfManagementFacilities, 0) AS numberOfManagementFacilities, IFNULL(r2.numberOfEducationalFacilities, 0) AS numberOfEducationalFacilities
+    FROM Ministries AS m
+    JOIN Persons AS p ON m.MinisterOfEducationID = p.PersonID
+    LEFT JOIN (
+      SELECT f.MinistryID, COUNT(f.FacilityID) AS numberOfManagementFacilities
+      FROM Facilities AS f
+      JOIN FacilityTypes AS ft ON f.FacilityTypeID = ft.TypeID
+      WHERE ft.TypeName = 'Management'
+      GROUP BY f.MinistryID
+    ) r on m.MinistryID = r.MinistryID
+    LEFT JOIN (
+      SELECT f.MinistryID, COUNT(f.FacilityID) AS numberOfEducationalFacilities
+      FROM Facilities AS f
+      JOIN FacilityTypes AS ft ON f.FacilityTypeID = ft.TypeID
+      WHERE ft.TypeName = 'Education'
+      GROUP BY f.MinistryID
+    ) r2 on m.MinistryID = r2.MinistryID
+    ORDER BY p.City ASC, r2.numberOfEducationalFacilities DESC;
+    ";
+    break;
+  case "q17":
+    $query = "
+SELECT p.PersonID, p.FirstName, p.LastName, MIN(er.StartDate) AS 'FirstDayAsTeacher', ft.TypeName AS 'Role', p.DateOfBirth, p.Email, SUM(TIMESTAMPDIFF(HOUR, s.startTime, s.endTime)) AS 'totalHours'
+FROM Persons p
+JOIN EmployeeRegistrations er ON p.PersonID = er.PersonID
+JOIN EmployeeType et ON er.EmployeeTypeID = et.EmployeeTypeID
+LEFT JOIN Specialization sp ON er.SpecializationID = sp.SpecializationID
+LEFT JOIN Schedules s ON p.PersonID = s.personID
+LEFT JOIN Facilities f ON er.FacilityID = f.FacilityID
+LEFT JOIN FacilityTypes ft ON f.FacilityTypeID = ft.TypeID
+WHERE 
+  et.EmployeeType = 'Teacher' 
+  AND sp.Specialization = 'School Counselor'
+  AND er.EndDate IS NULL
+  AND p.PersonID IN (
+    SELECT i.PersonID 
+    FROM Infections AS i
+    JOIN TypeOfInfections toi ON i.TypeID = toi.TypeID
+    WHERE toi.TypeName = 'COVID-19'
+    GROUP BY i.PersonID
+    HAVING COUNT(i.infectionID) >= 3
+  )
+GROUP BY p.PersonID, p.FirstName, p.LastName, ft.TypeName, p.DateOfBirth, p.Email
+ORDER BY ft.TypeName ASC, p.FirstName ASC, p.LastName ASC;
+    ";
+    break;
+}
+
+// Execute query if not empty
+if (!empty($query)) {
+  $statement = $conn->prepare($query);
+  if($statement->execute()) {
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+  } else {
+    header("Location: ./failure.php");
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles.css">
+  <script src="https://kit.fontawesome.com/6ebd7b3ed7.js" crossorigin="anonymous"></script>
+  <script>
+
+    //gathers the data from the table to be exported
+    function exportTableToCSV(filename) {
+            var csv = [];
+            var rows = document.querySelectorAll("table tr");
+            
+            for (var i = 0; i < rows.length; i++) {
+                var row = [], cols = rows[i].querySelectorAll("td, th");
+            for (var j = 0; j < cols.length; j++) 
+                row.push('"' + cols[j].innerText + '"');
+            csv.push(row.join(","));        
+            }
+            downloadCSV(csv.join("\n"), filename);
+        }
+            
+    //downloads the data as a csv file
+    function downloadCSV(csv, filename) {
+        var csvFile;
+        var downloadLink;
+        csvFile = new Blob([csv], {type: "text/csv"});
+        downloadLink = document.createElement("a");
+        downloadLink.download = filename;
+        downloadLink.href = window.URL.createObjectURL(csvFile);
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+    }
+  </script>
+  <title>Custom Queries</title>
+</head>
+<body>
+  <?php include_once ('../navbar.php'); ?>
+  <div class="containerLeftMargin">
+    <a class="topBtn" onclick="exportTableToCSV('Results.csv')">Export to CSV</a>
+    </div>
+  <div class="containerLeftMargin">
+    <h2>Results</h2>
+
+    <!-- Displays data from the selected custom query -->
+    <?php if (!empty($data)) { ?>
+    <table class="styled-table">
+    <thead>
+      <tr>
+        <?php foreach ($data[0] as $key => $value) { ?>
+          <td><?= $key?></td>
+        <?php } ?>
+        <?php unset($key); ?>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($data as $row) { ?>
+        <tr>
+          <?php foreach ($row as $key => $value) { ?>
+            <td><?= $value; ?></td>
+          <?php } ?>
+        </tr>
+      <?php } ?>
+    </tbody>
+    </table>
+    <?php } ?>
+
+    <button class="smallBtn" onclick="history.back()">Return to Previous Page</button>
+  </div>
+</body>
+</html>p
